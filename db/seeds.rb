e2d81a0f87a31fc5fe3a11fa765f2b7a935181d4
Coralie Delozanne <coralie.delozanne@gmail.com>
2020-05-14 12:50:15 +0200

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+
+# Ingredient.create(name: 'lemon')
+# Ingredient.create(name: 'ice')
+# Ingredient.create(name: 'mint leaves')
+# Ingredient.create(name: 'vodka')
+# Ingredient.create(name: 'gin')
+# Ingredient.create(name: 'cucumber')
+# Ingredient.create(name: 'violet sirup')
+# Ingredient.create(name: 'tonic')
+# Ingredient.create(name: 'aloe vera')
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients = JSON.parse(open(url).read)
+# puts ingredients['drinks']
+ingredients['drinks'].each do |ingredient|
+  Ingredient.create!(name: ingredient['strIngredient1'])
+end
